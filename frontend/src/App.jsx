9012640ad0911a9d2348a4ccce58b20ab559cfbb
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-10-04 23:32:57 +0530

    mini project using react and express

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
+
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/v1/data')
+      .then((response) => {
+        setData(response.data)
+        // console.log(response)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  })
 
   return (
     <>
-    <h1>Hello welcome mini project</h1>
+      <h1>Hello welcome mini project</h1>
+      <h2>Data: {data.length}</h2>
+      {
+        data.map((data) => (
+          <div key={data.id}>
+            <h3>{data.name}</h3>
+            <p>{data.favNumkeen}</p>
+          </div>
+        ))
+      }
     </>
   )
 }
